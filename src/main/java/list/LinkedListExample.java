122bc14d8cae41709b01d5aa9d4ac30a41af1483
Macy M <mei.mcanally@ttu.edu>
2021-02-10 16:37:38 -0600

    LinkedList examples

--- src/main/java/list/LinkedListExample.java
+++ src/main/java/list/LinkedListExample.java
@@ -1,10 +1,9 @@
 package list;
 
 import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+import sun.awt.image.ImageWatched;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class LinkedListExample {
 
@@ -12,6 +11,7 @@ public class LinkedListExample {
     LinkedList<String> a2 = new LinkedList<String>();
     a2.add("tiger");
     a2.add("monkey");
+    a2.add("sheep");
 
     //Append the specified element to the end of the linkedlist:
     LinkedList<String> animals = new LinkedList<String>();
@@ -93,6 +93,54 @@ public class LinkedListExample {
     animals.removeAll(animals);
     animals.clear();
     System.out.println("LinkedList after removing element: " + animals);
+
+    //Swap two elements in a linkedlist:
+    System.out.println("LinkedList before swapping: " + animals);
+    Collections.swap(animals, 0, 2);
+    System.out.println("LinkedList after swapping: " + animals);
+
+    //Shuffle the elements in a linkedlist:
+    System.out.println("LinkedList before swapping: " + animals);
+    Collections.shuffle(animals);
+    System.out.println("LinkedList after swapping: " + animals);
+
+    //Joining 2 linkedlists:
+    LinkedList<String> list = new LinkedList<String>();
+    list.addAll(animals);
+    list.addAll(a2);
+    System.out.println(list);
+
+    //Clone an linkedlist to another linkedlust:
+    LinkedList copylinkedlist = (LinkedList) animals.clone();
+    System.out.println(copylinkedlist);
+
+    //Remove and return the 1st element of the linkedlist:
+    System.out.println("LinkedList: " + animals);
+    System.out.println("1st element in the LinkedList is: " + animals.pop());
+    System.out.println("Linkedlist after removing element: " + animals);
+
+    //Retrieve but does not remove, the last element of a linkedlist:
+    System.out.println("LinkedLst: " + animals);
+    System.out.println("Showing the last element in the Linkedlist: " + animals.peekLast());
+
+    //Check if a particular element exits in a linkedlist:
+    System.out.println("LinkedList: " + animals);
+    if(animals.contains("cat")){
+      System.out.println("It Does!");
+    }else{
+      System.out.println("It doesn't");
+    }
+
+    //Convert a linkedlist into an arrauList:
+    List<String> arr = new ArrayList<String>(animals);
+    for (String s : arr)
+      System.out.println(s);
+
+    //Comparing 2 linkedlists:
+    LinkedList<String> c = new LinkedList<String>();
+    for (String x : animals)
+      c.add(a2.contains(x) ? "Yes" : "No");
+    System.out.println(c);
 
   }
 }
